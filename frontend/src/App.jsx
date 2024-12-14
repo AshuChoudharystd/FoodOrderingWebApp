84f7c2e76ccc7d5f8d2cd9f73e52a75ba4ddf8f3
AshuChoudharystd <146466303+AshuChoudharystd@users.noreply.github.com>
2024-12-14 22:03:16 +0530

    hero commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
@@ -5,17 +6,26 @@ import { Routes,Route} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
-    <div className='app'>
-      <Navbar/>
+    <div>
+      {showLogin ? <LoginPopup setShowLogin = {setShowLogin}/> : <></>}
+      <div className='app'>
+      <Navbar setShowLogin = {setShowLogin}/>
       <Routes>
         <Route path = '/' element={<Home/>}/>
         <Route path = '/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
-      <Footer/>
+    </div>
+    <br />
+    <br />
+    <Footer/>
     </div>
   )
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order Your Favourite Food Here</h2>
         <p>Our food delivery app brings your favorite dishes right to your doorstep, connecting you with a wide variety of local restaurants and cuisines.</p>
+        <a href='#explore-menu'>
         <button>View Menu</button>
+        </a>
       </div>
     </div>
   )
